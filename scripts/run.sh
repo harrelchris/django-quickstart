570ef6cb4f0dfaee103f5484ef2120517f935732
Chris Harrel <harrelchris@gmail.com>
2026-07-28 23:36:12 -0500

    Use Django server

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,8 @@ set -e
 
 source .venv/bin/activate
 
-python manage.py collectstatic --no-input
+python manage.py runserver
 
-python -m gunicorn --workers=1
+#python manage.py collectstatic --no-input
+
+#python -m gunicorn --workers=1
